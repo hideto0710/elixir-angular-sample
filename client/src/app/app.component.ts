bbd902799686c700d7264928f5927de17899bea8
hideto0710 <h.inamura0710@gmail.com>
2016-09-11 16:17:49 +0900

    クライアント側のコード整理

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Issue from "./issue";
 import './styles.css';
-
-interface Issue {
-  id: number;
-  url: string;
-  repositoryUrl: string;
-  labelsUrl: string;
-  commentsUrl: string;
-  htmlUrl: string;
-  number: number;
-  state: string;
-  title: string;
-  body: string;
-}
+import { Channel } from "phoenix";
+import { Socket } from "phoenix";
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   stacks: Issue[];
+  issueText: string;
+  messages: string[];
+  messageText: string;
+
+  channel: Channel;
 
   constructor() {
     this.stacks = (<any>window).IssuesGlobal || [];
+    this.issueText = '';
+    this.messages = [];
+    this.messageText = '';
+  }
+
+  ngOnInit() {
+    const socket = new Socket("/socket", { params: { token: '' } });
+    socket.connect();
+    this.channel = socket.channel("room:lobby", {});
+    this.channel.on("new_msg", payload => {
+      this.addMessage(`<br/>[${Date()}] ${payload.body}`);
+    });
+    this.channel.join()
+      .receive("ok", resp => { console.log("Joined successfully", resp) })
+      .receive("error", resp => { console.log("Unable to join", resp) });
+  }
+
+  addMessage(message: string) {
+    this.messages.push(message)
+  }
+
+  onMessageInputEnter(event: KeyboardEvent) {
+    this.channel.push("new_msg", {body: (<HTMLInputElement>event.target).value});
+    this.messageText = '';
+  }
+
+  onIssueInputEnter(event: KeyboardEvent) {
+    this.channel.push("new_issue", { body: (<HTMLInputElement>event.target).value });
+    this.issueText = '';
   }
 }
---
+++ client/src/app/issue.ts
@@ -0,0 +1,14 @@
+class Issue {
+  id: number;
+  url: string;
+  repositoryUrl: string;
+  labelsUrl: string;
+  commentsUrl: string;
+  htmlUrl: string;
+  number: number;
+  state: string;
+  title: string;
+  body: string;
+}
+
+export default Issue;
